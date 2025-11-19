bde7bce0be50a672ec43906358d3654e888a576f
praytoo <praytoo52@icloud.com>
2025-11-19 12:27:48 -0800

    update

--- src/main/java/com/pluralsight/NorthwindManagementQueries/N5.sql
+++ src/main/java/com/pluralsight/NorthwindManagementQueries/N5.sql
@@ -1,1 +1,6 @@
 N5. Slow Shipping Detection Management asks: “Where are we slow in shipping?” Create a report that, for each ship country, shows the average delay between OrderDate and ShippedDate (you may ignore orders that haven’t shipped yet). Only show countries with a noticeably higher average delay than others.
+SELECT AVG(DATEDIFF(o.OrderDate, o.ShippedDate)), o.ShipCountry
+FROM northwind.orders AS o
+GROUP BY ShipCountry
+i. -11.0000	Ireland
+   -10.2162	Sweden
